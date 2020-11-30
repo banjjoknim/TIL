0e8922e8c5c0a5c2a7d4eada791f1302bb1880d5
DESKTOP-FSO9NHB\User <zhfxmtkachdt@daum.net>
2020-11-30 11:49:13 +0900

    Revert "스프링 부트와 AWS로 혼자 구현하는 웹 서비스 Chapter5 추가"
    This reverts commit cd2f0f6e78d98e11657efc7f51e2d4cd2da0d6d6.

--- WebServiceBySpringBootAndAWS/WebServiceBySpringBootAndAWS.md
+++ WebServiceBySpringBootAndAWS/WebServiceBySpringBootAndAWS.md
@@ -4,4 +4,3 @@
 - [Chapter2. 스프링 부트에서 테스트 코드를 작성하자](https://github.com/banjjoknim/TIL/blob/master/WebServiceBySpringBootAndAWS/src/ChapterDescription/Chapter2.md)
 - [Chapter3. 스프링 부트에서 JPA로 데이터베이스 다뤄보자](https://github.com/banjjoknim/TIL/blob/master/WebServiceBySpringBootAndAWS/src/ChapterDescription/Chapter3.md)
 - [Chapter4. 머스테치로 화면 구성하기](https://github.com/banjjoknim/TIL/blob/master/WebServiceBySpringBootAndAWS/src/ChapterDescription/Chapter4.md)
-- [Chapter5. 스프링 시큐리티와  OAuth 2.0으로 로그인 기능 구현하기](https://github.com/banjjoknim/TIL/blob/master/WebServiceBySpringBootAndAWS/src/ChapterDescription/Chapter5.md)
--- WebServiceBySpringBootAndAWS/src/main/java/com/banjjoknim/book/springboot/web/IndexController.java
+++ WebServiceBySpringBootAndAWS/src/main/java/com/banjjoknim/book/springboot/web/IndexController.java
@@ -1,7 +1,5 @@
 package com.banjjoknim.book.springboot.web;
 
-import com.banjjoknim.book.springboot.config.auth.LoginUser;
-import com.banjjoknim.book.springboot.config.auth.dto.SessionUser;
 import com.banjjoknim.book.springboot.service.PostsService;
 import com.banjjoknim.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,22 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user) {
+    public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        if (user != null) {
-            model.addAttribute("userName", user.getName());
-        }
         return "index";
     }
 
