eb3903fcf88d33e4103e06dd72872bdd28c0c54b
banjjoknim <zhfxmtkachdt@daum.net>
2020-12-23 19:39:46 +0900

    [김영한님의 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술] 스프링 DB 접근 기술 - '스프링 통합 테스트' 수강

--- SpringBoot-Introduction/Lectures/Contents.md
+++ SpringBoot-Introduction/Lectures/Contents.md
@@ -30,3 +30,4 @@
 ## 스프링 DB 접근 기술
 - [H2 데이터베이스 설치](https://github.com/banjjoknim/TIL/blob/master/SpringBoot-Introduction/Lectures/Lecture18.md)
 - [순수 JDBC](https://github.com/banjjoknim/TIL/blob/master/SpringBoot-Introduction/Lectures/Lecture19.md)
+- [스프링 통합 테스트](https://github.com/banjjoknim/TIL/blob/master/SpringBoot-Introduction/Lectures/Lecture20.md)
---
+++ SpringBoot-Introduction/src/test/java/hello/springintroduction/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,53 @@
+package hello.springintroduction.service;
+
+import hello.springintroduction.domain.Member;
+import hello.springintroduction.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 존재하는 회원입니다.");
+
+        // then
+
+    }
+
+}
