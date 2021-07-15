67d6d4d290b72883514e651193c8ba06b4ac8ed1
banjjoknim <zhfxmtkachdt@daum.net>
2021-07-15 22:02:17 +0900

    [2장. 객체 생성과 파괴] - '아이템 6. 불필요한 객체 생성을 피하라' 추가

---
+++ 이펙티브 자바 (Effective Java)/src/chapter2/item6/Sum.java
@@ -0,0 +1,11 @@
+package chapter2.item6;
+
+class Sum {
+    private static long sum() {
+        Long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
