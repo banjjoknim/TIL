d40f396229a46ef4eafc6ba4b2eed05e1a7d8b7d
WIN10-01\UserK <zhfxmtkachdt@daum.net>
2021-07-26 02:32:51 +0900

    [4장. 클래스와 인터페이스] - '아이템 17. 변경 가능성을 최소화하라' 추가

---
+++ 이펙티브 자바 (Effective Java)/src/chapter4/item17/Complex.java
@@ -0,0 +1,49 @@
+package chapter4.item17;
+
+import java.util.Objects;
+
+final class Complex {
+    private final double re;
+    private final double im;
+
+//    public Complex(double re, double im) {
+//        this.re = re;
+//        this.im = im;
+//    }
+
+    private Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
+    public double realPart() {
+        return re;
+    }
+
+    public double imaginaryPart() {
+        return im;
+    }
+
+    public Complex plus(Complex complex) {
+        return new Complex(re + complex.re, im + complex.im);
+    }
+
+    // ...
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Complex complex = (Complex) o;
+        return Double.compare(complex.re, re) == 0 && Double.compare(complex.im, im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(re, im);
+    }
+}
