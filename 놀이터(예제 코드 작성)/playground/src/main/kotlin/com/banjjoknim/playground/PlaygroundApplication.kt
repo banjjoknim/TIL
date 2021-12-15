50596410b9ef8233aa3026bfa125d5c41b01d8d1
배재홍 <jhbae@carmerce.co.kr>
2021-12-15 16:25:50 +0900

    playground project initial commit

---
+++ 놀이터(예제 코드 작성)/playground/src/main/kotlin/com/banjjoknim/playground/PlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.banjjoknim.playground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlaygroundApplication>(*args)
+}
