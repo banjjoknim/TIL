6392cb8c6d51584ec133670fd752095b569af8d1
banjjoknim <zhfxmtkachdt@daum.net>
2022-01-19 10:36:34 +0900

    chore : add Spring Security Configuration

---
+++ 놀이터(예제 코드 작성)/playground/src/main/kotlin/com/banjjoknim/playground/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.banjjoknim.playground.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+            .antMatchers("/user/**").authenticated()
+            .antMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().permitAll()
+            .and()
+            .formLogin()
+            .loginPage("/login");
+    }
+}
