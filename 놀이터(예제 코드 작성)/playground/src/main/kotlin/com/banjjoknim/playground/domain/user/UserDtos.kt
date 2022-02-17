4aea11b833211dbdc55236731f2c967e8ac2616e
banjjoknim <zhfxmtkachdt@daum.net>
2022-02-17 20:30:09 +0900

    refactor : add default value

--- 놀이터(예제 코드 작성)/playground/src/main/kotlin/com/banjjoknim/playground/domain/user/UserDtos.kt
+++ 놀이터(예제 코드 작성)/playground/src/main/kotlin/com/banjjoknim/playground/domain/user/UserDtos.kt
@@ -4,11 +4,11 @@ import javax.validation.constraints.NotBlank
 
 data class CreateUserRequest(
     @field:NotBlank(message = "이름을 입력해주세요")
-    val name: String,
+    val name: String = "",
     @field:NotBlank(message = "이메일을 입력해주세요")
-    val email: String,
+    val email: String = "",
     @field:NotBlank(message = "휴대폰 번호를 입력해주세요")
-    val phoneNumber: String
+    val phoneNumber: String = ""
 ) {
     fun toUser(): User {
         return User(name = name, email = email, phoneNumber = phoneNumber)
