1e0696deba788d83917b7e461d59520984e4b4f1
banjjoknim <zhfxmtkachdt@daum.net>
2022-03-11 21:25:17 +0900

    feat : jaskson 프로젝트 추가

---
+++ 놀이터(예제 코드 작성)/jackson/src/test/kotlin/com/banjjoknim/playground/JacksonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banjjoknim.playground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JacksonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
