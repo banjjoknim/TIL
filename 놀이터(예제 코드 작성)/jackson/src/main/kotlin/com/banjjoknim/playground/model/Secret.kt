160781432f6abef2996f7a8d64044d694c12dd96
banjjoknim <zhfxmtkachdt@daum.net>
2022-03-12 15:21:30 +0900

    feat : 커스텀 직렬화에 사용할 Secret 어노테이션 추가

---
+++ 놀이터(예제 코드 작성)/jackson/src/main/kotlin/com/banjjoknim/playground/model/Secret.kt
@@ -0,0 +1,14 @@
+package com.banjjoknim.playground.model
+
+import com.fasterxml.jackson.annotation.JacksonAnnotation
+
+/**
+ * [jackson-annotations](https://www.baeldung.com/jackson-annotations) 참고.
+ *
+ * @see com.fasterxml.jackson.annotation.JacksonAnnotation
+ * @see com.fasterxml.jackson.annotation.JacksonAnnotationsInside
+ */
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.PROPERTY)
+@JacksonAnnotation // Jackson 에서 이 어노테이션을 인식할 수 있게 만들어준다.
+annotation class Secret
