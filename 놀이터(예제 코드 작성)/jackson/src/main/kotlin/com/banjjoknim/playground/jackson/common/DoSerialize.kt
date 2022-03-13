abcf7da69e5209757b8622790313f076767b0aef
banjjoknim <zhfxmtkachdt@daum.net>
2022-03-13 13:39:49 +0900

    refactor : 불필요한 어노테이션 삭제

--- 놀이터(예제 코드 작성)/jackson/src/main/kotlin/com/banjjoknim/playground/jackson/common/DoSerialize.kt
+++
@@ -1,5 +0,0 @@
-package com.banjjoknim.playground.jackson.common
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.PROPERTY)
-annotation class DoSerialize(val doSerialize: Boolean = false)
