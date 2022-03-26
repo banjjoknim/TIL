cc1bd398fb82a7d3ac1adfa889afb790afcff5c2
banjjoknim <zhfxmtkachdt@daum.net>
2022-03-26 18:56:23 +0900

    chore : @CrossOrigin 어노테이션 관련 주석 추가

--- 놀이터(예제 코드 작성)/spring-security/src/main/kotlin/com/banjjoknim/playground/domain/home/HomeApiController.kt
+++ 놀이터(예제 코드 작성)/spring-security/src/main/kotlin/com/banjjoknim/playground/domain/home/HomeApiController.kt
@@ -3,6 +3,14 @@ package com.banjjoknim.playground.domain.home
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * `@CrossOrigin` 어노테이션을 사용하더라도 Security 인증이 필요한 요청은 전부 거부된다.
+ *
+ * `@CrossOrigin` 어노테이션은 인증이 필요하지 않은 요청만 허용해준다.
+ *
+ * @see org.springframework.web.bind.annotation.CrossOrigin
+ */
+//@CrossOrigin("*")
 @RestController
 class HomeApiController {
 
