ba26a7d1381ca1e2d37b6444769d43178ba38d94
banjjoknim <zhfxmtkachdt@daum.net>
2022-03-26 17:54:07 +0900

    feat : JwtUser Entity 추가

---
+++ 놀이터(예제 코드 작성)/spring-security/src/main/kotlin/com/banjjoknim/playground/domain/user/JwtUser.kt
@@ -0,0 +1,23 @@
+package com.banjjoknim.playground.domain.user
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class JwtUser(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0L,
+    var username: String = "",
+    var password: String = "",
+    var roles: String = "" // USER, ADMIN
+) {
+    fun getRoles(): List<String> {
+        if (roles.isNotEmpty()) {
+            return roles.split(",")
+        }
+        return emptyList()
+    }
+}
