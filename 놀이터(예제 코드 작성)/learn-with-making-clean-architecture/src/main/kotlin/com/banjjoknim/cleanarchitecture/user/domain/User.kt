b7785205c0851f853e3dba55233208d6d67de23c
banjjoknim <zhfxmtkachdt@daum.net>
2022-04-22 00:42:44 +0900

    refactor(User) : 닉네임 변경 함수 이름 수정

--- 놀이터(예제 코드 작성)/learn-with-making-clean-architecture/src/main/kotlin/com/banjjoknim/cleanarchitecture/user/domain/User.kt
+++ 놀이터(예제 코드 작성)/learn-with-making-clean-architecture/src/main/kotlin/com/banjjoknim/cleanarchitecture/user/domain/User.kt
@@ -4,7 +4,7 @@ class User(
     var id: Long = 0L,
     var nickname: Nickname
 ) {
-    fun changeName(newNickname: String) {
+    fun changeNickname(newNickname: String) {
         this.nickname = Nickname(newNickname)
     }
 }
