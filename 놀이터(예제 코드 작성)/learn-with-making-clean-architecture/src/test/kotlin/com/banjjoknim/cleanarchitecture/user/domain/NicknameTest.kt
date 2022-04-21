7f06cf39b5a5dde90824d25c023d44513e282dff
banjjoknim <zhfxmtkachdt@daum.net>
2022-04-22 00:56:38 +0900

    test(user.domain) : 회원 닉네임 변경 테스트 추가

---
+++ 놀이터(예제 코드 작성)/learn-with-making-clean-architecture/src/test/kotlin/com/banjjoknim/cleanarchitecture/user/domain/NicknameTest.kt
@@ -0,0 +1,24 @@
+package com.banjjoknim.cleanarchitecture.user.domain
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
+class NicknameTest {
+
+    @DisplayName("닉네임 생성 테스트")
+    @Nested
+    inner class ChangeNicknameTestCases {
+        @Test
+        fun `10글자 이내이면 닉네임을 생성할 수 있다`() {
+            assertDoesNotThrow { Nickname("banjjoknim") }
+        }
+
+        @Test
+        fun `10글자가 넘으면 닉네임을 생성할 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> { Nickname("i'm banjjoknim") }
+        }
+    }
+}
