0da78fab0feebf4a26ae0978e30ae6b6d8ebe033
banjjoknim <zhfxmtkachdt@daum.net>
2022-04-24 17:51:31 +0900

    refactor : UserEntity @Table 이름 적용

--- 놀이터(예제 코드 작성)/learn-with-making-clean-architecture/src/main/kotlin/com/banjjoknim/cleanarchitecture/user/adapter/out/persistence/UserEntity.kt
+++ 놀이터(예제 코드 작성)/learn-with-making-clean-architecture/src/main/kotlin/com/banjjoknim/cleanarchitecture/user/adapter/out/persistence/UserEntity.kt
@@ -3,12 +3,14 @@ package com.banjjoknim.cleanarchitecture.user.adapter.out.persistence
 import javax.persistence.Embedded
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 
 /**
  * 패키지를 독립적 배포의 관점에서 바라봤을 때, UserEntity 는 Persistence 영역에 존재하는 것이 타당하다.
  *
  * 만약 그렇지 않다면 UserEntity 가 존재하지 않는 UserEntityRepository 가 배포될 것이기 때문이다.
  */
+@Table(name = "User")
 @Entity
 class UserEntity(
     @Id
