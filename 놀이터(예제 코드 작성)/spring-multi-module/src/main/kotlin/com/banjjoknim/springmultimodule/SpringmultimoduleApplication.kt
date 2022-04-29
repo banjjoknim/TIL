f8f4c968380efa0eb6e680d50e588de0e2c92f47
banjjoknim <zhfxmtkachdt@daum.net>
2022-04-29 22:52:15 +0900

    initial commit

---
+++ 놀이터(예제 코드 작성)/spring-multi-module/src/main/kotlin/com/banjjoknim/springmultimodule/SpringmultimoduleApplication.kt
@@ -0,0 +1,11 @@
+package com.banjjoknim.springmultimodule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringmultimoduleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringmultimoduleApplication>(*args)
+}
---
+++ 놀이터(예제 코드 작성)/spring-multi-module/src/test/kotlin/com/banjjoknim/springmultimodule/SpringmultimoduleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banjjoknim.springmultimodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringmultimoduleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
