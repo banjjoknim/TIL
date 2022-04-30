fad4085d00f9acd759bd1c3ce2c713a7d56a0f8d
banjjoknim <zhfxmtkachdt@daum.net>
2022-05-01 02:18:47 +0900

    feat : module-api 에 SpringBootApplication 실행 파일 추가

---
+++ 놀이터(예제 코드 작성)/spring-multi-module/module-api/src/test/kotlin/com/banjjoknim/springmultimodule/SpringMultiModuleApplicationKtTest.kt
@@ -0,0 +1,12 @@
+package com.banjjoknim.springmultimodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringMultiModuleApplicationKtTest {
+
+    @Test
+    fun contextLoads() {
+    }
+}
