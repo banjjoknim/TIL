eb505572a17fef94fecbfd9dd6844a011db2711b
banjjoknim <zhfxmtkachdt@daum.net>
2022-08-04 12:14:07 +0900

    refactor : Long, LocalDateTime 타입 프로퍼티 추가

--- 놀이터(예제 코드 작성)/graphql-kotlin/src/main/kotlin/com/banjjoknim/graphqlkotlin/person/Person.kt
+++ 놀이터(예제 코드 작성)/graphql-kotlin/src/main/kotlin/com/banjjoknim/graphqlkotlin/person/Person.kt
@@ -1,5 +1,9 @@
 package com.banjjoknim.graphqlkotlin.person
 
+import java.time.LocalDateTime
+
 data class Person(
     var name: String,
+    var age: Long? = 0L,
+    var birthDate: LocalDateTime = LocalDateTime.now()
 )
