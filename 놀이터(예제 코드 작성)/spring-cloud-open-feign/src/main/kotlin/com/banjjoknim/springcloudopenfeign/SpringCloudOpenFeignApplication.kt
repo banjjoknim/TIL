a91fb579cf41081bab199216009d76a019f30df3
Colt <zhfxmtkachdt@daum.net>
2022-10-28 18:54:21 +0900

    initial commit

---
+++ 놀이터(예제 코드 작성)/spring-cloud-open-feign/src/main/kotlin/com/banjjoknim/springcloudopenfeign/SpringCloudOpenFeignApplication.kt
@@ -0,0 +1,11 @@
+package com.banjjoknim.springcloudopenfeign
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudOpenFeignApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCloudOpenFeignApplication>(*args)
+}
---
+++ 놀이터(예제 코드 작성)/spring-cloud-open-feign/src/test/kotlin/com/banjjoknim/springcloudopenfeign/SpringCloudOpenFeignApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banjjoknim.springcloudopenfeign
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudOpenFeignApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
