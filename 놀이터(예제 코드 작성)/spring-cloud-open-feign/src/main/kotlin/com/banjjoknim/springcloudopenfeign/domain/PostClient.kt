2a8480d2f4919cb519f94bce2a55b05cef697bd5
Colt <zhfxmtkachdt@daum.net>
2022-10-28 20:09:13 +0900

    refactor : 게시글 클라이언트 컴포넌트 value 변경

--- 놀이터(예제 코드 작성)/spring-cloud-open-feign/src/main/kotlin/com/banjjoknim/springcloudopenfeign/domain/PostClient.kt
+++ 놀이터(예제 코드 작성)/spring-cloud-open-feign/src/main/kotlin/com/banjjoknim/springcloudopenfeign/domain/PostClient.kt
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(value = "postClient", url = "https://jsonplaceholder.typicode.com/")
+@FeignClient(value = "post", url = "https://jsonplaceholder.typicode.com/")
 interface PostClient {
 
     @GetMapping("/posts")
