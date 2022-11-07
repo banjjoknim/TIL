8ac2fd7efffa53a99e83c1f72733876274fff86a
banjjoknim <zhfxmtkachdt@daum.net>
2022-11-07 13:10:45 +0900

    initial commit

---
+++ 놀이터(예제 코드 작성)/solid-design-pattern-sample/src/main/kotlin/com/banjjoknim/soliddesignpatternsample/SolidDesignPatternSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.banjjoknim.soliddesignpatternsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SolidDesignPatternSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SolidDesignPatternSampleApplication>(*args)
+}
---
+++ 놀이터(예제 코드 작성)/solid-design-pattern-sample/src/test/kotlin/com/banjjoknim/soliddesignpatternsample/SolidDesignPatternSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banjjoknim.soliddesignpatternsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SolidDesignPatternSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
