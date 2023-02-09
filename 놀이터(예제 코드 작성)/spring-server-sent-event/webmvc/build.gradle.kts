31fb615fb8f46d76358aafafc4156247648407d4
banjjoknim <zhfxmtkachdt@daum.net>
2023-02-09 13:35:26 +0900

    initial commit

---
+++ 놀이터(예제 코드 작성)/spring-server-sent-event/webmvc/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.banjjoknim"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
