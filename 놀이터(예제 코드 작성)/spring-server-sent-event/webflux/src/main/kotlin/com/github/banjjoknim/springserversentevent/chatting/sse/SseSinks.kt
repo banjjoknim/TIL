3877779d3b1019ae427bc066ff000965aa50e3e4
banjjoknim <zhfxmtkachdt@daum.net>
2023-02-10 16:07:04 +0900

    feat : webflux SSE 관련 컴포넌트 추가

---
+++ 놀이터(예제 코드 작성)/spring-server-sent-event/webflux/src/main/kotlin/com/github/banjjoknim/springserversentevent/chatting/sse/SseSinks.kt
@@ -0,0 +1,34 @@
+package com.github.banjjoknim.springserversentevent.chatting.sse
+
+import mu.KLogging
+import org.springframework.http.codec.ServerSentEvent
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Sinks
+import java.time.LocalDateTime
+
+@Component
+object SseSinks {
+    private val logger = KLogging().logger
+    private val sinks = Sinks.many().multicast().directBestEffort<SseData>()
+
+    fun registerSse(): Flux<ServerSentEvent<SseData>> {
+        return sinks.asFlux()
+            .doOnError { error -> logger.error { "$error occured in SseSinks#registerSse()" } }
+            .flatMap { it.toServerSentEvent() }
+            .defaultIfEmpty(ServerSentEvent.builder(SseData.DEFAULT).build())
+    }
+
+    private fun SseData.toServerSentEvent(): Flux<ServerSentEvent<SseData>> {
+        val serverSentEvent = ServerSentEvent.builder<SseData>()
+            .id("${LocalDateTime.now()}")
+            .event(eventName)
+            .data(this)
+            .build()
+        return Flux.just(serverSentEvent)
+    }
+
+    fun emitData(sseData: SseData) {
+        sinks.tryEmitNext(sseData)
+    }
+}
