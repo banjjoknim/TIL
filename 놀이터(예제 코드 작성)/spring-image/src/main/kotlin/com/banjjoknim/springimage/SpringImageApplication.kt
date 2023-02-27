79de1a69c157c2d1d607004e72f9755a0b394c3e
banjjoknim <zhfxmtkachdt@daum.net>
2023-02-27 11:07:38 +0900

    initial commit

---
+++ 놀이터(예제 코드 작성)/spring-image/src/main/kotlin/com/banjjoknim/springimage/SpringImageApplication.kt
@@ -0,0 +1,11 @@
+package com.banjjoknim.springimage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringImageApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringImageApplication>(*args)
+}
---
+++ 놀이터(예제 코드 작성)/spring-image/src/test/kotlin/com/banjjoknim/springimage/SpringImageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banjjoknim.springimage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringImageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
