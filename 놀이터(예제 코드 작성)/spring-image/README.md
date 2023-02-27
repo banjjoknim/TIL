6c9fde65addd34c9d2b17ddaf499f56a25a23d15
banjjoknim <zhfxmtkachdt@daum.net>
2023-02-27 13:32:18 +0900

    feat : 이미지 업로드 구현

---
+++ 놀이터(예제 코드 작성)/spring-image/README.md
@@ -0,0 +1,14 @@
+# Spring Image
+
+이미지 처리의 기본적인 것들을 구현한다.
+
+## 이미지 업로드
+
+## 이미지 다운로드
+
+## 이미지 삭제
+
+## 썸네일 생성
+
+- webp 타입 처리
+- [webp-imageio plugin](https://github.com/sejda-pdf/webp-imageio)
---
+++ 놀이터(예제 코드 작성)/spring-image/src/main/kotlin/com/banjjoknim/springimage/api/ImageDtos.kt
@@ -0,0 +1,20 @@
+package com.banjjoknim.springimage.api
+
+import org.springframework.web.multipart.MultipartFile
+
+data class UploadImageRequest(
+    val files: MultipartFile,
+)
+
+data class UploadImageResponse(
+    val name: String,
+    val path: String,
+)
+
+data class UploadMultiImageRequest(
+    val files: List<MultipartFile>,
+)
+
+data class UploadMultiImageResponse(
+    val nameAndPaths: List<UploadImageResponse>
+)
