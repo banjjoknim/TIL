c576938a2c65493c21336354278d93d23fd09576
banjjoknim <zhfxmtkachdt@daum.net>
2023-03-17 17:22:27 +0900

    build : JOOQ code generation 설정 추가

--- 놀이터(예제 코드 작성)/spring-jooq-liquibase/build.gradle.kts
+++ 놀이터(예제 코드 작성)/spring-jooq-liquibase/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	id("nu.studer.jooq") version "7.1.1"
 }
 
 group = "com.banjjoknim"
@@ -21,8 +22,16 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.liquibase:liquibase-core")
-	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// mysql
+	runtimeOnly("com.mysql:mysql-connector-j:8.0.32")
+	jooqGenerator("com.mysql:mysql-connector-j")
+
+	// h2
+	runtimeOnly("com.h2database:h2")
+	jooqGenerator("com.h2database:h2")
+	jooqGenerator("jakarta.xml.bind:jakarta.xml.bind-api:3.0.1") // java.lang.NoClassDefFoundError: jakarta/xml/bind/annotation/XmlSchema 추가되었음
 }
 
 tasks.withType<KotlinCompile> {
@@ -34,4 +43,78 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	version.set("3.16.4")  // default (can be omitted)
+	edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)  // default (can be omitted)
+	configurations {
+		create("main") {  // name of the jOOQ configuration
+			generateSchemaSourceOnCompilation.set(true)  // default (can be omitted)
+
+			// H2 Code Generation 설정
+			jooqConfiguration.apply {
+				logging = org.jooq.meta.jaxb.Logging.WARN
+				jdbc.apply {
+					driver = "org.h2.Driver"
+					url = "jdbc:h2:~/test;AUTO_SERVER=TRUE;MODE=MySQL;"
+					user = "sa"
+					password = ""
+				}
+				generator.apply {
+					name = "org.jooq.codegen.KotlinGenerator"
+					database.apply {
+						name = "org.jooq.meta.h2.H2Database"
+						includes = ".*"
+						excludes = ""
+					}
+					target.apply {
+						packageName = "com.jooq.entity"
+						directory = "src/main/kotlin/jooq"  // default (can be omitted)
+					}
+					generate.apply {
+						isDeprecated = false
+						isRecords = true
+						isImmutablePojos = true
+						isFluentSetters = true
+						isJavaTimeTypes = true
+						isJavaTimeTypes = true
+						// Generate POJOs as data classes, when using the KotlinGenerator. Default is true.
+						isPojosAsKotlinDataClasses = true
+					}
+				}
+
+				// MySQL Code Generation 설정
+//				jdbc.apply {
+//					driver = "com.mysql.cj.jdbc.Driver"
+//					url = "jdbc:mysql://127.0.0.1:3306"
+//					user = "root"
+//					password = ""
+//				}
+//				generator.apply {
+//					name = "org.jooq.codegen.KotlinGenerator"
+//					database.apply {
+//						name = "org.jooq.meta.mysql.MySQLDatabase"
+//						inputSchema = "test"
+//						isOutputSchemaToDefault = true
+//					}
+//					generate.apply {
+//						isDeprecated = false
+//						isRecords = true
+//						isImmutablePojos = true
+//						isFluentSetters = true
+//						isJavaTimeTypes = true
+
+//                      // Generate POJOs as data classes, when using the KotlinGenerator. Default is true.
+//						isPojosAsKotlinDataClasses = true
+//					}
+//					target.apply {
+//						packageName = "com.jooq.entity"
+//						directory = "src/main/kotlin/jooq"  // default (can be omitted)
+//					}
+//					strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+//				}
+			}
+		}
+	}
 }
