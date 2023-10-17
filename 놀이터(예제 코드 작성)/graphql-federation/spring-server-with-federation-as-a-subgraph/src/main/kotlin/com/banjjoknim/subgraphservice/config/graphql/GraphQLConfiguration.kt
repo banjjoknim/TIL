a0f330a81272ac706018e4dc2d8694df68d9c5fb
banjjoknim <zhfxmtkachdt@daum.net>
2023-10-17 18:37:32 +0900

    [graphql-federation-subgraph] docs : `GraphQL Federation` 설정 관련 라이브러리 클래스 링크 주석 추가

--- 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subgraph/src/main/kotlin/com/banjjoknim/subgraphservice/config/graphql/GraphQLConfiguration.kt
+++ 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subgraph/src/main/kotlin/com/banjjoknim/subgraphservice/config/graphql/GraphQLConfiguration.kt
@@ -22,11 +22,34 @@ class GraphQLConfiguration {
         return object : Schema {}
     }
 
+    /**
+     *
+     * ```graphql
+     * query {
+     *   _service {
+     *     sdl
+     *   }
+     * }
+     * ```
+     *
+     * 참고 : Federation 설정이 진행되면 위 쿼리를 이용해서 SuperGraph 에 SubGraph를 제공할 수 있게 됨.
+     *
+     * @see com.expediagroup.graphql.generator.SchemaGenerator
+     * @see com.expediagroup.graphql.generator.SchemaGeneratorConfig
+     * @see com.expediagroup.graphql.generator.hooks.SchemaGeneratorHooks
+     * @see com.expediagroup.graphql.generator.hooks.NoopSchemaGeneratorHooks
+     * @see com.expediagroup.graphql.server.spring.NonFederatedSchemaAutoConfiguration
+     *
+     * @see com.expediagroup.graphql.generator.federation.FederatedSchemaGenerator
+     * @see com.expediagroup.graphql.generator.federation.FederatedSchemaGeneratorConfig
+     * @see com.expediagroup.graphql.generator.federation.FederatedSchemaGeneratorHooks
+     * @see com.expediagroup.graphql.generator.federation.FederatedSchemaGeneratorHooks.willBuildSchema // 이 함수에서 Federation 관련 설정이 진행됨.
+     * @see com.expediagroup.graphql.generator.federation.types.SERVICE_OBJECT_TYPE // 상단에 명시된 쿼리 참조.
+     */
     @Bean
     fun federatedSchemaGeneratorHooks(resolvers: Optional<List<FederatedTypeResolver>>): FederatedSchemaGeneratorHooks {
         return CustomFederationSchemaGeneratorHooks(resolvers.orElse(emptyList()))
     }
-
 
     @Bean
     fun graphQL(schema: GraphQLSchema?): GraphQL {
