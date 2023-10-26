0a742d6971fb8079ac05f6ba83fc949905fc713e
banjjoknim <zhfxmtkachdt@daum.net>
2023-10-26 14:19:45 +0900

    [graphql-federation-subscription] initial commit

---
+++ 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subscription/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-server-with-federation-as-a-subscription"
---
+++ 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subscription/src/main/kotlin/com/banjjoknim/subscriptionservice/SubscriptionserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.banjjoknim.subscriptionservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubscriptionserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubscriptionserviceApplication>(*args)
+}
