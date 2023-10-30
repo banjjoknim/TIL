543bd57d8101331278580fbcdafa976e1ee5bae7
banjjoknim <zhfxmtkachdt@daum.net>
2023-10-30 11:08:12 +0900

    [spring-server-with-federation] docs : `RedisConfiguration` 주석 내용 수정

--- 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subgraph/src/main/kotlin/com/banjjoknim/subgraphservice/config/redis/RedisConfiguration.kt
+++ 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subgraph/src/main/kotlin/com/banjjoknim/subgraphservice/config/redis/RedisConfiguration.kt
@@ -1,7 +1,9 @@
 package com.banjjoknim.subgraphservice.config.redis
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
@@ -18,7 +20,7 @@ class RedisConfiguration {
 
     /**
      * ```
-     * @Bean 주석처리 이유.
+     * @Primary 처리 이유.
      * -> Parameter 0 of method reactiveStringRedisTemplate in org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration required a single bean, but 2 were found:
      * 	- connectionFactory: defined by method 'connectionFactory' in class path resource [com/banjjoknim/subgraphservice/config/redis/RedisConfiguration.class]
      * 	- redisConnectionFactory: defined by method 'redisConnectionFactory' in class path resource [org/springframework/boot/autoconfigure/data/redis/LettuceConnectionConfiguration.class]
@@ -30,7 +32,8 @@ class RedisConfiguration {
      * @see org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
      * @see org.springframework.boot.autoconfigure.data.redis.LettuceConnectionConfiguration // RedisConnectionFactory 타입 Bean이 존재하지 않을 경우 자동으로 redisConnectionFactory Bean을 등록함.
      */
-//    @Bean // 주석처리에 대해서는 상기 doc에 기재된 내용 참조.
+    @Primary // 상기 doc에 기재된 내용 참조.
+    @Bean
     fun connectionFactory(): ReactiveRedisConnectionFactory {
         return LettuceConnectionFactory(RedisStandaloneConfiguration(redisHost, redisPort.toInt()))
     }
