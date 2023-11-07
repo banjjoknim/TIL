c3f1ad5bf7445491cfcbc289e2cb5e8cdd144a5f
banjjoknim <zhfxmtkachdt@daum.net>
2023-11-07 15:31:59 +0900

    [spring-server-with-federation-as-a-subgraph] refactor : `Kafka` 관련 설정 구현 및 `spring.profile`에 따라 각각의 설정 분리

---
+++ 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subgraph/src/main/kotlin/com/banjjoknim/subgraphservice/config/messaging/kafka/KafkaConfiguration.kt
@@ -0,0 +1,48 @@
+package com.banjjoknim.subgraphservice.config.messaging.kafka
+
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaAdmin
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Profile("kafka")
+@Configuration
+class KafkaConfiguration {
+
+    @Value(value = "\${spring.kafka.bootstrap-servers}")
+    private lateinit var bootstrapAddress: String
+
+    @Bean
+    fun kafkaAdmin(): KafkaAdmin {
+        val configs: MutableMap<String, Any?> = HashMap()
+        configs[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
+        return KafkaAdmin(configs)
+    }
+
+    @Bean
+    fun topic(): NewTopic {
+        return NewTopic("pickupBook", 1, 1.toShort())
+    }
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> {
+        val configProps: MutableMap<String, Any> = HashMap()
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> {
+        return KafkaTemplate(producerFactory())
+    }
+}
