55ed31e024bf760b600745674ba4cbfdcf8ecc81
banjjoknim <zhfxmtkachdt@daum.net>
2023-11-07 19:06:00 +0900

    [spring-server-with-federation-as-a-subgraph] refactor : `Kafka Topic` 생성시 `numPartitions` 값 변경 및 주석 추가

--- 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subgraph/src/main/kotlin/com/banjjoknim/subgraphservice/domain/book/infrastructure/messaging/kafka/KafkaTopicConfiguration.kt
+++ 놀이터(예제 코드 작성)/graphql-federation/spring-server-with-federation-as-a-subgraph/src/main/kotlin/com/banjjoknim/subgraphservice/domain/book/infrastructure/messaging/kafka/KafkaTopicConfiguration.kt
@@ -16,6 +16,12 @@ class KafkaTopicConfiguration {
      */
     @Bean
     fun pickupBookTopic(): NewTopic {
-        return NewTopic("pickupBook", 0, 1.toShort())
+        /**
+         * numPartitions의 값은 Partition의 ID와 무관한 값임. 해당 Topic에 대해 Partition의 갯수를 몇개로 할지 설정하는 것. 그에 따라 0으로 설정해도 무조건 1개 이상의 Partition이 생성됨.
+         */
+        val topicName = "pickupBook"
+        val numPartitions = 1
+        val replicationFactor = 1.toShort()
+        return NewTopic(topicName, numPartitions, replicationFactor)
     }
 }
