c7f82066b04a02567f853e8838a2cf72566d7f9f
jhbae <zhfxmtkachdt@daum.net>
2024-10-30 17:53:26 +0900

    [argocd-sample] initial commit

---
+++ playground/argocd-sample/src/main/kotlin/com/banjjoknim/argocdsample/ArgocdSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.banjjoknim.argocdsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArgocdSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArgocdSampleApplication>(*args)
+}
