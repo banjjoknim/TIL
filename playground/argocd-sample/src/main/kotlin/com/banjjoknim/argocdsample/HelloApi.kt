f60c1bd7f9381e47950494a5e2517f1bf8a3a2d8
jhbae <zhfxmtkachdt@daum.net>
2024-11-05 14:37:45 +0900

    [argocd-sample] config : application, Dockerfile, K8s 설정 파일 작성

--- playground/argocd-sample/src/main/kotlin/com/banjjoknim/argocdsample/HelloApi.kt
+++ playground/argocd-sample/src/main/kotlin/com/banjjoknim/argocdsample/HelloApi.kt
@@ -1,13 +1,20 @@
 package com.banjjoknim.argocdsample
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloApi {
 
+    @Value("\${APPLICATION_VERSION}")
+    private lateinit var applicationVersion: String
+
+    @Value("\${SECRET_NAME}")
+    private lateinit var secretName: String
+
     @GetMapping("/hello")
     fun hello(): String {
-        return "Argo!!"
+        return "hello Argo! my name is... [$secretName]. applicationVersion: [$applicationVersion]"
     }
 }
