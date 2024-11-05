1b41c26d59bf9cd6f08a69a9273b747f388fe07c
banjjoknim <zhfxmtkachdt@daum.net>
2024-11-05 16:24:42 +0900

    Argocd sample (#27)
    * [argocd-sample] initial commit
    
    * [argocd-sample] feat : health check API 작성
    
    * [argocd-sample] config : application, Dockerfile, K8s 설정 파일 작성

---
+++ playground/argocd-sample/src/test/kotlin/com/banjjoknim/argocdsample/ArgocdSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banjjoknim.argocdsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArgocdSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
