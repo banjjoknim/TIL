3414fd9b45a6ef2397eb94ef90e0f86b9bb5c9a4
banjjoknim <zhfxmtkachdt@daum.net>
2025-03-27 11:14:47 +0900

    [spring-cloud-gateway] feat : reactive-server-1 내부 모듈 추가

---
+++ playground/spring-cloud-gateway/spring-cloud-gateway-reactive/reactive-server-1/src/test/kotlin/com/banjjoknim/reactiveserver1/ReactiveServer1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banjjoknim.reactiveserver1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveServer1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
