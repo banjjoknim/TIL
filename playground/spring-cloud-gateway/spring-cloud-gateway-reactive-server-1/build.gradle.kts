2bab218cc01319899375309f365f62afb4878997
banjjoknim <zhfxmtkachdt@daum.net>
2025-03-28 13:39:32 +0900

    [spring-cloud-gateway] build : 내부 모듈 자바 버전 지정(17)

--- playground/spring-cloud-gateway/spring-cloud-gateway-reactive-server-1/build.gradle.kts
+++ playground/spring-cloud-gateway/spring-cloud-gateway-reactive-server-1/build.gradle.kts
@@ -9,6 +9,8 @@ group = "com.banjjoknim"
 version = "0.0.1-SNAPSHOT"
 
 java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
