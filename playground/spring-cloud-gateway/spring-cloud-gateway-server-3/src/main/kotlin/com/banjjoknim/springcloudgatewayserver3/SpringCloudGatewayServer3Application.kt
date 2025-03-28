fe3a03a037e3f7311e6a8cad5e7c7c2853b08bed
banjjoknim <zhfxmtkachdt@daum.net>
2025-03-28 14:49:09 +0900

    [spring-cloud-gateway] feat : `spring-cloud-gateway-server-3` 모듈 작성

---
+++ playground/spring-cloud-gateway/spring-cloud-gateway-server-3/src/main/kotlin/com/banjjoknim/springcloudgatewayserver3/SpringCloudGatewayServer3Application.kt
@@ -0,0 +1,11 @@
+package com.banjjoknim.springcloudgatewayserver3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudGatewayServer3Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudGatewayServer3Application>(*args)
+}
