944267ea632739f93f7a362a1f7c5d9bb6565065
jhbae <zhfxmtkachdt@daum.net>
2025-04-11 13:37:51 +0900

    [spring-cloud-gateway] build : 하위모듈 include 설정 추가

--- playground/spring-cloud-gateway/settings.gradle.kts
+++ playground/spring-cloud-gateway/settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "spring-cloud-gateway"
+include("spring-cloud-gateway-mvc")
+include("spring-cloud-gateway-reactive")
+include("spring-cloud-gateway-server-1")
+include("spring-cloud-gateway-server-2")
+include("spring-cloud-gateway-server-3")
